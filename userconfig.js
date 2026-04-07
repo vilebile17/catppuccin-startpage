19e18fab01b12b6756e86897d887d524206204e5
Ibrahim Somauroo <ibrahimsomauroo@proton.me>
2026-04-07 14:50:46 +0100

    Never gonna let you down

--- userconfig.js
+++ userconfig.js
@@ -31,7 +31,7 @@ const default_configuration = {
   disabled: [],
   localIcons: true,
   localFonts: true,
-  fastlink: "https://www.perplexity.ai",
+  fastlink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
   openLastVisitedTab: true,
   tabs: [
     {
